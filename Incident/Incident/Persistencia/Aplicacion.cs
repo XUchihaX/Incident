084e8b3ef0def293da2b76f4e532d46e12a5a4a3
Unknown <theracingj@gmail.com>
2018-04-14 18:55:19 -0400

    Primer commit de la aplicacion Incident.
    Commit #1

---
+++ Incident/Incident/Persistencia/Aplicacion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace Incident.Persistencia
+{
+    class Aplicacion
+    {
+
+        public static bool Insertar(string aplicacion)
+        {
+            string query = string.Empty;
+            bool respuesta = false;
+
+            query = string.Format("insert into aplicacion (aplicacion) values ({0})",aplicacion);
+
+            MySqlConnection conecion = new MySqlConnection(Conexion.Conec());
+            MySqlCommand comando = new MySqlCommand(query,conecion);
+            conecion.Open();
+
+            var filasAfectadas = comando.ExecuteNonQuery();
+            
+            if (filasAfectadas > 0)
+                respuesta = true;
+
+            return respuesta;
+        }
+
+        public static bool Actualizar(string aplicacion)
+        {
+            string query = string.Empty;
+            bool respuesta = false;
+
+            query = string.Format("update aplicacion set aplicacion = '{0}' ", aplicacion);
+
+            MySqlConnection conecion = new MySqlConnection(Conexion.Conec());
+            MySqlCommand comando = new MySqlCommand(query, conecion);
+            conecion.Open();
+
+            var filasAfectadas = comando.ExecuteNonQuery();
+
+            if (filasAfectadas > 0)
+                respuesta = true;
+
+            return respuesta;
+        }
+
+        public static DataTable Aplicaciones()
+        {
+            string query = string.Empty;
+            DataTable tbl = new DataTable();
+
+            query = string.Format("select * from aplicacion where estado = true order by aplicacion_id");
+            MySqlConnection conecion = new MySqlConnection(Conexion.Conec());
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, conecion);
+
+            adapter.Fill(tbl);
+
+            return tbl;
+        }
+
+    }
+}
---
+++ Incident/Incident/Persistencia/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incident.Persistencia
+{
+    class Conexion
+    {
+        static string strCon = string.Empty;
+
+        public static string Conec()
+        {
+            strCon = "server=localhost;user id=root;database=incidencias";
+
+            return strCon;
+        }
+    }
+}
